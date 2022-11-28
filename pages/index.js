9aad56aeef18438d844cc40c880f4408d1a19e67
Continuous Integration <thierryskoda@users.noreply.github.com>
2022-11-28 11:26:33 +0200

    working solution travel

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head"
+import Image from "next/image"
+import { useState } from "react"
+import buildspaceLogo from "../assets/buildspace-logo.png"
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("")
+  const [apiOutput, setApiOutput] = useState("")
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value)
+    setUserInput(event.target.value)
+  }
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true)
+
+    console.log("Calling OpenAI...")
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    })
+
+    const data = await response.json()
+    const { output } = data
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`)
+    setIsGenerating(false)
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,13 +41,51 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Your perfect travel itinerary</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>
+              Input where you wanna go, for how long and what type of travel you
+              want to do and we'll generate you a prefect itinerary just for
+              you!
+            </h2>
+          </div>
+          <div className="download-extension-wrapper">
+            <p>
+              Like what you see? <a>Download the chrome extension</a> 🎉
+            </p>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="Type the itinerary you want to generate here..."
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+                {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+        </div>
+
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
+
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
@@ -31,7 +99,7 @@ const Home = () => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
